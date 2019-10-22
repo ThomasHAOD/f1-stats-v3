e137702a1773d9438c98a5a877a3a26ffa45c2a4
TamHOD <thomashodonnell@gmail.com>
2019-10-22 14:46:44 +0100

    adjust checkbox styling

--- src/Containers/OptionsContainer.js
+++ src/Containers/OptionsContainer.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class OptionsContainer extends Component {
-    render() {
-        return (
-            <div className="options-container">
-                <h1>Options Go Here</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="options-container">
+        <h1>Options Go Here</h1>
+
+        <div class="slide">
+          <input
+            type="checkbox"
+            value="None"
+            id="slide"
+            name="check"
+            checked
+          />
+          <label for="slide"></label>
+        </div>
+        <div class="slide">
+          <input
+            type="checkbox"
+            value="None"
+            id="slide"
+            name="check"
+            checked
+          />
+          <label for="slideTwo"></label>
+        </div>
+        <div class="slide">
+          <input
+            type="checkbox"
+            value="None"
+            id="slide"
+            name="check"
+            checked
+          />
+          <label for="slide"></label>
+        </div>
+      </div>
+    );
+  }
 }
