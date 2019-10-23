8bf65f4139018280f6fb35222705ffed68fb0055
TamHOD <thomashodonnell@gmail.com>
2019-10-23 14:48:21 +0100

    reformat options to be more readable

--- src/Containers/OptionsContainer.js
+++ src/Containers/OptionsContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import Dropdown from 'react-bootstrap/Dropdown'
 import CheckBox from '../Components/OptionComponents/CheckBox'
 
@@ -8,7 +9,6 @@ export default class OptionsContainer extends Component {
   render() {
     return (
       <div className="options-container">
-          <br/>
         <h3>Include Retirements</h3><CheckBox/>
         <br/>
         <h3>Include Wet Races</h3><CheckBox/>
@@ -17,28 +17,30 @@ export default class OptionsContainer extends Component {
         <br/>
 
         
-        
-        <DropdownButton id="dropdown-basic-button" title="Select Track Type" variant="info">
+        <ButtonGroup vertical>
+        <DropdownButton className="options-dropdown" id="dropdown-basic-button" title="Select Track Type" variant="info">
           <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
         </DropdownButton>
-        <DropdownButton id="dropdown-basic-button" title="Select Track" variant="info">
+        <DropdownButton className="options-dropdown" id="dropdown-basic-button" title="Select Track" variant="info">
           <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
         </DropdownButton>
 
-        <DropdownButton id="dropdown-basic-button" title="Select Constructor" variant="info">
+        <DropdownButton  className="options-dropdown" id="dropdown-basic-button" title="Select Constructor" variant="info">
           <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
         </DropdownButton>
-        <DropdownButton id="dropdown-basic-button" title="Select Year" variant="info">
+        <DropdownButton className="options-dropdown" id="dropdown-basic-button" title="Select Year" variant="info">
           <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
         </DropdownButton>
+        </ButtonGroup>
+        
         
         <h3>Comparison Mode</h3><CheckBox/>
       </div>
