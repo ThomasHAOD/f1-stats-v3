2cff5a0c6755b9b5fd39bd290d468e9023075783
TamHOD <thomashodonnell@gmail.com>
2019-11-05 19:52:22 +0000

    change formatting of driver main details

--- src/Components/ImageComponents/MainImage.js
+++ src/Components/ImageComponents/MainImage.js
@@ -3,10 +3,7 @@ import Image from 'react-bootstrap/Image'
 
 const MainImage = (props) => {
     return (
-        <div className="image-and-subtitle">
         <Image src="./logo192.png" roundedCircle fluid/>
-        <h1>HAM:44</h1>
-        </div>
         
     )
 }
--- src/Components/MainDetailsComponents/DriverMainDetails.js
+++ src/Components/MainDetailsComponents/DriverMainDetails.js
@@ -4,6 +4,7 @@ const DriverMainDetails = (props) => {
     return (
         <div className="main-details" id="driver-main-details">
             <h1>Lewis Hamilton</h1>
+            <h1>HAM:44</h1>
             <h2>34</h2>
             <h2>British</h2>
         </div>
