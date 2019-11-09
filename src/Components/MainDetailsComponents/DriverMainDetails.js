bcf1a9543f4f2f1258de1c3eaa1d3142bfaa75f2
TamHOD <thomashodonnell@gmail.com>
2019-11-09 15:11:42 +0000

    add conditional statement to clean up frontend

--- src/Components/MainDetailsComponents/DriverMainDetails.js
+++ src/Components/MainDetailsComponents/DriverMainDetails.js
@@ -1,16 +1,20 @@
 import React from "react";
 
 const DriverMainDetails = props => {
-  return (
-    <div className="main-details" id="driver-main-details">
-      <h1>{props.driver1Name}</h1>
-      <h1>
-        {props.driver1Code}:{props.driver1Number}
-      </h1>
-      <h2>34</h2>
-      <h2>{props.driver1Nationality}</h2>
-    </div>
-  );
+  if (props.driver1Code) {
+    return (
+      <div className="main-details" id="driver-main-details">
+        <h1>{props.driver1Name}</h1>
+        <h1>
+          {props.driver1Code}:{props.driver1Number}
+        </h1>
+        <h2>34</h2>
+        <h2>{props.driver1Nationality}</h2>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default DriverMainDetails;
